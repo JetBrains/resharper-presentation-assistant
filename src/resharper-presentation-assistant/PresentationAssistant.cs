7830116003113f62fc9d1ade330400a9500afcc9
Matt Ellis <m.t.ellis@gmail.com>
2015-01-06 10:41:32 +0000

    Keyboard modifiers shown in "keyboard" style

--- src/resharper-presentation-assistant/PresentationAssistant.cs
+++ src/resharper-presentation-assistant/PresentationAssistant.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Windows.Forms;
 using JetBrains.ActionManagement;
 using JetBrains.Application;
-using JetBrains.Application.BuildScript.Application;
 using JetBrains.Application.Parts;
 using JetBrains.DataFlow;
 using JetBrains.UI.ActionsRevised.Handlers;
 using JetBrains.UI.ActionsRevised.Loader;
 using JetBrains.UI.ActionsRevised.Shortcuts;
-using JetBrains.UI.ActionSystem;
 using JetBrains.UI.PopupMenu.Impl;
 using JetBrains.Util;
 
@@ -18,71 +15,107 @@ namespace JetBrains.ReSharper.Plugins.PresentationAssistant
     [ShellComponent(Requirement = InstantiationRequirement.Instant)]
     public class PresentationAssistant
     {
-        private readonly OutputPanelLogger logger;
         private readonly PresentationAssistantWindowOwner presentationAssistantWindowOwner;
         private readonly IActionShortcuts actionShortcuts;
 
-        public PresentationAssistant(Lifetime lifetime, ActionEvents actionEvents, OutputPanelLogger logger, PresentationAssistantWindowOwner presentationAssistantWindowOwner, IActionShortcuts actionShortcuts, IActionDefs defs)
+        public PresentationAssistant(Lifetime lifetime, ActionEvents actionEvents,
+            PresentationAssistantWindowOwner presentationAssistantWindowOwner, IActionShortcuts actionShortcuts, IActionDefs defs)
         {
-            this.logger = logger;
             this.presentationAssistantWindowOwner = presentationAssistantWindowOwner;
             this.actionShortcuts = actionShortcuts;
             actionEvents.AdviseExecuteAction(lifetime, OnAction);
-
-            var ids = defs.GetAllActionDefs().Where(d => !string.IsNullOrEmpty(d.Description));
-            foreach (var id in ids)
-            {
-                logger.Log(id.Text + " " + id.Description);
-            }
         }
 
         private void OnAction(ActionEvents.ActionEventArgs obj)
         {
             // Would be nice to use IActionShortcuts here, but that only returns for a single scheme
-            var vsShortcut = string.Empty;
-            var intellijShortcut = string.Empty;
 
             // TODO: Remove trailing ellipsis
             var text = MnemonicStore.RemoveMnemonicMark(obj.ActionDef.Text);
             text = string.IsNullOrEmpty(text) ? obj.ActionDef.ActionId : text;
 
-            if (obj.ActionDef.IdeaShortcuts != null)
-            {
-                // Remove near duplicate chords (e.g. Control+R R, Control+R Control+R
-                // Replace "Control" with "Ctrl", or an icon of the key
-                //foreach (var s in obj.ActionDef.IdeaShortcuts)
-                //{
-                //    var s2 = ShortcutUtil.ParseKeyboardShortcut(s);
-                //    var shortcuts = s2.KeyboardShortcuts;
-                //    if (shortcuts[0].Key == shortcuts[1].Key)
-                //}
-                //ActionShortcut keyboardShortcut = ShortcutUtil.ParseKeyboardShortcut(obj.ActionDef.IdeaShortcuts[0]);
-
-                intellijShortcut = string.Join(", ", obj.ActionDef.IdeaShortcuts);
-            }
-
-            if (obj.ActionDef.VsShortcuts != null)
-                vsShortcut = string.Join(", ", obj.ActionDef.VsShortcuts);
-
+            // TODO: Only report the first shortcut
+            // ReSharper registers chords twice, once where the second char doesn't have modifiers
+            // and once where the second char repeats the modifier of the first char.
+            // E.g. Ctrl+R, R and Ctrl+R, Ctrl+R. This allows for flexibility in hitting that chord
+            // (do you hold Ctrl down all the time, or just for the first char? Doesn't matter!)
+            // Empirically, there are only two actions that have a genuine alternative shortcut -
+            // SafeDelete (VS): Ctrl+R, D and Alt+Delete
+            // Rename (IntelliJ): F2 and Shift+F6
+            // These can be safely ignored, meaning we can just show the primary shortcut
             var shortcut = new Shortcut
             {
                 Text = text,
                 Description = obj.ActionDef.Description,
-                VsShortcut = vsShortcut,
-                IntellijShortcut = intellijShortcut,
+                VsShortcuts = GetShortcuts(obj.ActionDef.VsShortcuts),
+                IntellijShortcuts = GetShortcuts(obj.ActionDef.IdeaShortcuts),
                 CurrentScheme = actionShortcuts.CurrentScheme
             };
 
             presentationAssistantWindowOwner.Show(shortcut);
         }
+
+        // TODO: This is getting expensive in terms of allocations...
+        private ShortcutSequence[] GetShortcuts(string[] shortcuts)
+        {
+            // TODO: Remove near-duplicates (e.g. Control+R R, Control+R Control+R)
+            var parsedShortcuts = from s in shortcuts ?? EmptyArray<string>.Instance
+                let parsed = ShortcutUtil.ParseKeyboardShortcut(s)
+                let details = from k in parsed.KeyboardShortcuts
+                    select new ShortcutDetails(GetKey(k.Key), k.Modifiers)
+                select new ShortcutSequence(details.ToArray());
+            return parsedShortcuts.ToArray();
+        }
+
+        private string GetKey(Keys key)
+        {
+            var converter = new KeysConverter();
+            return converter.ConvertToString(key);
+        }
+    }
+
+    public class ShortcutSequence
+    {
+        public ShortcutSequence(params ShortcutDetails[] details)
+        {
+            Details = details;
+        }
+
+        public ShortcutDetails[] Details { get; private set; }
+    }
+
+    public class ShortcutDetails
+    {
+        public ShortcutDetails(string key, KeyboardModifiers modifiers = KeyboardModifiers.None)
+        {
+            Key = key;
+            HasAlt = (modifiers & KeyboardModifiers.Alt) != 0;
+            HasControl = (modifiers & KeyboardModifiers.Control) != 0;
+            HasShift = (modifiers & KeyboardModifiers.Shift) != 0;
+        }
+
+        public string Key { get; set; }
+        public bool HasAlt { get; set; }
+        public bool HasControl { get; set; }
+        public bool HasShift { get; set; }
     }
 
     public class Shortcut
     {
         public string Text { get; set; }
-        public string Description { get; set; }
-        public string VsShortcut { get; set; }
-        public string IntellijShortcut { get; set; }
+        public string Description { get; set; } // Only used by 1 action in ReSharper!
+        public ShortcutSequence[] VsShortcuts { get; set; }
+        public ShortcutSequence[] IntellijShortcuts { get; set; }
         public ShortcutScheme CurrentScheme { get; set; }
+
+        public bool HasShortcuts
+        {
+            get { return VsShortcuts.Length > 0; }
+        }
+
+        public bool HasIntellijShortcuts
+        {
+            get { return IntellijShortcuts.Length > 0; }
+        }
     }
 }
--- src/resharper-presentation-assistant/PresentationAssistantWindow.xaml.cs
+++ src/resharper-presentation-assistant/PresentationAssistantWindow.xaml.cs
@@ -1,11 +1,11 @@
-﻿using JetBrains.UI.RichText;
+﻿using System.Windows.Forms;
+using JetBrains.ActionManagement;
+using JetBrains.UI.RichText;
 
 namespace JetBrains.ReSharper.Plugins.PresentationAssistant
 {
     public partial class PresentationAssistantWindow
     {
-        private static readonly TextStyle Bold = new TextStyle(System.Drawing.FontStyle.Bold);
-
         public PresentationAssistantWindow()
         {
             InitializeComponent();
@@ -13,17 +13,51 @@ namespace JetBrains.ReSharper.Plugins.PresentationAssistant
 
         public void SetShortcut(Shortcut shortcut)
         {
-            var richText = new RichText();
-            richText.Append(shortcut.Text, Bold);
-            if (!string.IsNullOrEmpty(shortcut.VsShortcut))
-            {
-                richText.Append(" via ", TextStyle.Default);
-                richText.Append(shortcut.VsShortcut);
-                if (!string.IsNullOrEmpty(shortcut.IntellijShortcut) && shortcut.VsShortcut != shortcut.IntellijShortcut)
-                    richText.Append(string.Format(" ({0} for IntelliJ)", shortcut.IntellijShortcut));
-            }
+            DataContext = shortcut;
+        }
+    }
 
-            DataContext = richText;
+    public static class SampleData
+    {
+        public static Shortcut SimpleShortcut
+        {
+            get
+            {
+                return new Shortcut
+                {
+                    Text = "Highlight Usages in File",
+                    VsShortcuts = new []{new ShortcutSequence(new ShortcutDetails("F11", KeyboardModifiers.Shift | KeyboardModifiers.Alt))},
+                    IntellijShortcuts = new[] { new ShortcutSequence(new ShortcutDetails("F7", KeyboardModifiers.Control | KeyboardModifiers.Shift)) }
+                };
+            }
+        }
+
+        public static Shortcut ChordShortcut
+        {
+            get
+            {
+                var ctrlR = new ShortcutDetails("R", KeyboardModifiers.Control);
+                return new Shortcut
+                {
+                    Text = "Run Unit Tests",
+                    VsShortcuts = new[] { new ShortcutSequence(ctrlR, new ShortcutDetails("T")) },
+                    IntellijShortcuts = new[] { new ShortcutSequence(ctrlR, new ShortcutDetails("T")) }
+                };
+            }
+        }
+
+        public static Shortcut MultipleShortcut
+        {
+            get
+            {
+                var ctrlR = new ShortcutDetails("R", KeyboardModifiers.Control);
+                return new Shortcut()
+                {
+                    Text = "Rename",
+                    VsShortcuts = new []{ new ShortcutSequence(ctrlR, new ShortcutDetails("R")), new ShortcutSequence(ctrlR, ctrlR) },
+                    IntellijShortcuts = new []{ new ShortcutSequence(new ShortcutDetails("F2")), new ShortcutSequence(new ShortcutDetails("F6", KeyboardModifiers.Shift)) }
+                };
+            }
         }
     }
 }
