df83cb675c6dbc04ecce0216b5bf0dfbb8428b33
Matt Ellis <m.t.ellis@gmail.com>
2015-01-21 11:59:46 +0000

    Handle "go to" overrides

--- src/resharper-presentation-assistant/ShortcutFactory.cs
+++ src/resharper-presentation-assistant/ShortcutFactory.cs
@@ -1,6 +1,8 @@
 using JetBrains.ActionManagement;
 using JetBrains.Application;
 using JetBrains.ReSharper.Feature.Services.Util;
+using JetBrains.ReSharper.Features.Navigation.Features.GoToDeclaration;
+using JetBrains.ReSharper.Features.Navigation.Features.GoToImplementation;
 using JetBrains.UI.ActionsRevised.Loader;
 using JetBrains.UI.ActionsRevised.Shortcuts;
 using JetBrains.UI.PopupMenu.Impl;
@@ -13,6 +15,7 @@ namespace JetBrains.ReSharper.Plugins.PresentationAssistant
         private static readonly char[] TrimCharacters = {'.', '\u2026'};
 
         private readonly IActionShortcuts actionShortcuts;
+        private readonly IActionDefs defs;
         private readonly VsShortcutFinder vsShortcutFinder;
         private readonly ActionPresentationHelper actionPresentationHelper;
 
@@ -20,12 +23,16 @@ namespace JetBrains.ReSharper.Plugins.PresentationAssistant
                                VsShortcutFinder vsShortcutFinder, ActionPresentationHelper actionPresentationHelper)
         {
             this.actionShortcuts = actionShortcuts;
+            this.defs = defs;
             this.vsShortcutFinder = vsShortcutFinder;
             this.actionPresentationHelper = actionPresentationHelper;
         }
 
         public Shortcut Create(IActionDefWithId def, int multiplier)
         {
+            IActionDefWithId overridingDef;
+            def = GetPrimaryDef(def, out overridingDef);
+
             var shortcut = new Shortcut
             {
                 ActionId = def.ActionId,
@@ -36,8 +43,44 @@ namespace JetBrains.ReSharper.Plugins.PresentationAssistant
                 Multiplier = multiplier
             };
 
-            SetShortcuts(shortcut, def);
+            SetShortcuts(shortcut, def, overridingDef);
             return shortcut;
+        }
+
+        private IActionDefWithId GetPrimaryDef(IActionDefWithId originalDef, out IActionDefWithId secondaryDef)
+        {
+            // The way ReSharper overrides Visual Studio's go to methods is a bit confusing.
+            // Normally, an overridden command is just an action that also overrides a VS
+            // command. The go to commands have both ReSharper actions and VS overriding 
+            // commands. Also, the names are confusing. Visual Studio's "Go to Definition"
+            // maps to ReSharper "Go to Declaration" and its "Go to Declaration" maps to
+            // "Go to Implementation". The IntelliJ shortcuts are specified in the ReSharper
+            // actions, and the VS shortcuts are simply what's mapped to the overridden
+            // command. So, we need to handle the two shortcuts at the same time - one
+            // will provide the standard name, and the IntelliJ shortcut, the other will
+            // provide the details to get to the overridden VS command to give us the
+            // proper bindings.
+            switch (originalDef.ActionId)
+            {
+                case "GotoDefinitionOverride":
+                    secondaryDef = originalDef;
+                    return defs.GetActionDef<GotoDeclarationAction>();
+
+                case GotoDeclarationAction.ACTION_ID:
+                    secondaryDef = defs.GetActionDef<GotoDefinitionOverrideAction>();
+                    return originalDef;
+
+                case "GoToDeclarationOverride":
+                    secondaryDef = originalDef;
+                    return defs.GetActionDef<GotoImplementationsAction>();
+
+                case GotoImplementationsAction.GOTO_IMPLEMENTATION_ACTION_ID:
+                    secondaryDef = defs.GetActionDef<GoToDeclarationOverrideAction>();
+                    return originalDef;
+            }
+
+            secondaryDef = originalDef;
+            return originalDef;
         }
 
         private string GetPath(IActionDefWithId def)
@@ -69,13 +112,13 @@ namespace JetBrains.ReSharper.Plugins.PresentationAssistant
             return text;
         }
 
-        private void SetShortcuts(Shortcut shortcut, IActionDefWithId def)
+        private void SetShortcuts(Shortcut shortcut, IActionDefWithId def, IActionDefWithId overridingDef)
         {
             // TODO: Should this be an option in the options dialog? Show secondary scheme if different?
             const bool showSecondarySchemeIfSame = false;
 
             SetGivenShortcuts(shortcut, def, showSecondarySchemeIfSame);
-            SetVsOverriddenShortcuts(shortcut, def, showSecondarySchemeIfSame);
+            SetVsOverriddenShortcuts(shortcut, overridingDef, showSecondarySchemeIfSame);
             SetWellKnownShortcuts(shortcut, def, showSecondarySchemeIfSame);
         }
 
@@ -170,6 +213,19 @@ namespace JetBrains.ReSharper.Plugins.PresentationAssistant
                     // Only happens when we're tabbing around hotspots in Live Templates. Useful to show
                 case "TextControl.Tab":
                     shortcut.VsShortcut = GetShortcutSequence("Tab");
+                    break;
+
+                    // The shortcuts for the overridden VS "go to" commands come from the current
+                    // binding, but if we're in the IntelliJ binding, they get removed, so we have
+                    // to hard code them.
+                case "GotoDeclaration":
+                    if (shortcut.VsShortcut == null)
+                        shortcut.VsShortcut = GetShortcutSequence("F12");
+                    break;
+
+                case "GotoImplementation":
+                    if (shortcut.VsShortcut == null)
+                        shortcut.VsShortcut = GetShortcutSequence("Control+F12");
                     break;
             }
 
