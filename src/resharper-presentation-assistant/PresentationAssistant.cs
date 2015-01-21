e2e7de4ea585a0d9f3af3145dd295e853027edc1
Matt Ellis <m.t.ellis@gmail.com>
2015-01-21 14:51:46 +0000

    Track actions from workflow menus
    E.g. Navigate To, Refactor This, Generate
    
    Fixes #3

--- src/resharper-presentation-assistant/PresentationAssistant.cs
+++ src/resharper-presentation-assistant/PresentationAssistant.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using JetBrains.Application;
+using JetBrains.Application.ActivityTrackingNew;
 using JetBrains.Application.Parts;
 using JetBrains.DataFlow;
-using JetBrains.UI.ActionsRevised.Handlers;
 using JetBrains.UI.ActionsRevised.Loader;
 
 namespace JetBrains.ReSharper.Plugins.PresentationAssistant
 {
     [ShellComponent(Requirement = InstantiationRequirement.Instant)]
-    public class PresentationAssistant
+    public class PresentationAssistant : IActivityTracking
     {
         private static readonly TimeSpan MultiplierTimeout = TimeSpan.FromSeconds(10);
 
+        private readonly IActionDefs defs;
         private readonly PresentationAssistantWindowOwner presentationAssistantWindowOwner;
         private readonly ShortcutFactory shortcutFactory;
 
@@ -19,22 +20,38 @@ namespace JetBrains.ReSharper.Plugins.PresentationAssistant
         private string lastActionId;
         private int multiplier;
 
-        public PresentationAssistant(Lifetime lifetime, ActionEvents actionEvents,
+        public PresentationAssistant(Lifetime lifetime, IActionDefs defs,
                                      PresentationAssistantWindowOwner presentationAssistantWindowOwner,
                                      ShortcutFactory shortcutFactory)
         {
+            this.defs = defs;
             this.presentationAssistantWindowOwner = presentationAssistantWindowOwner;
             this.shortcutFactory = shortcutFactory;
-            actionEvents.AdviseExecuteAction(lifetime, OnAction);
         }
 
-        private void OnAction(ActionEvents.ActionEventArgs obj)
+        // Implementing IActivityTracking is better than subscribing to ActionEvents, as
+        // extensible workflow based actions (Refactor This, Navigate To, Generate, etc.)
+        // will notify activity tracking, but invoke the action manually, so it doesn't
+        // get reported by ActionEvents
+        public void TrackAction(string actionId)
         {
-            if (ActionIdBlacklist.IsBlacklisted(obj.ActionDef.ActionId))
+            var def = defs.TryGetActionDefById(actionId);
+            if (def != null)
+                OnAction(def);
+        }
+
+        public void TrackActivity(string activityGroup, string activityId, int count = 1)
+        {
+            // TODO: Track activities in VsAction activityGroup
+        }
+
+        private void OnAction(IActionDefWithId def)
+        {
+            if (ActionIdBlacklist.IsBlacklisted(def.ActionId))
                 return;
 
-            UpdateMultiplier(obj.ActionDef);
-            var shortcut = shortcutFactory.Create(obj.ActionDef, multiplier);
+            UpdateMultiplier(def);
+            var shortcut = shortcutFactory.Create(def, multiplier);
             presentationAssistantWindowOwner.Show(shortcut);
         }
 
