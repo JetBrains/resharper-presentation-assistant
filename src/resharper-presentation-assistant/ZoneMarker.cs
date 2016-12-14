02df195c72e55aaee65133d6d45b348146117e12
Matt Ellis <m.t.ellis@gmail.com>
2016-12-14 10:16:50 +0000

    Support ReSharper 2016.3

--- src/resharper-presentation-assistant/ZoneMarker.cs
+++ src/resharper-presentation-assistant/ZoneMarker.cs
@@ -1,10 +1,11 @@
 using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services.Daemon;
 
 namespace JetBrains.ReSharper.Plugins.PresentationAssistant
 {
     // TODO: Proper zone requirements
     [ZoneMarker]
-    public class ZoneMarker
+    public class ZoneMarker : IRequire<DaemonZone>
     {
     }
 }
