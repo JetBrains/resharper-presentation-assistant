6c6f2a4cd7ac12af044f97548c58fcef3dae8b62
Matthias Koch <ithrowexceptions@gmail.com>
2021-12-08 18:38:31 +0100

    Add JetBrains.Platform.VisualStudio.SinceVs11 package and ISinceVs11FrontEnvZone

--- src/dotnet/JetBrains.PresentationAssistant/VisualStudio/ZoneMarker.cs
+++ src/dotnet/JetBrains.PresentationAssistant/VisualStudio/ZoneMarker.cs
@@ -1,10 +1,11 @@
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.Platform.VisualStudio.SinceVs11.Shell.Zones;
 using JetBrains.VsIntegration.Shell.Zones;
 
 namespace JetBrains.ReSharper.Plugins.PresentationAssistant.VisualStudio
 {
     [ZoneMarker]
-    public class ZoneMarker : IRequire<IVisualStudioFrontendEnvZone>
+    public class ZoneMarker : IRequire<IVisualStudioFrontendEnvZone>, IRequire<ISinceVs11FrontEnvZone>
     {
     }
 }
